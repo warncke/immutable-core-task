27f2732603c8ff78b4c11f1079f930b55c367afc
Ersun Warncke <ersun.warncke@gmail.com>
2017-11-08 12:58:58 -0400

    wip

---
+++ test/run-error-check.js
@@ -0,0 +1,217 @@
+'use strict'
+
+/* npm modules */
+const ImmutableAI = require('immutable-ai')
+const ImmutableCore = require('immutable-core')
+const ImmutableDatabaseMariaSQL = require('immutable-database-mariasql')
+const ImmutableGlobal = require('immutable-global')
+const ImmutableCoreModel = require('immutable-core-model')
+const Promise = require('bluebird')
+const chai = require('chai')
+const sinon = require('sinon')
+
+/* application modules */
+const ImmutableCoreTask = require('../lib/immutable-core-task')
+const ImmutableCoreTaskInstance = require('../lib/immutable-core-task-instance')
+const initModels = require('./lib/init-models')
+
+/* chai config */
+const assert = chai.assert
+sinon.assert.expose(chai.assert, { prefix: '' })
+
+const dbHost = process.env.DB_HOST || 'localhost'
+const dbName = process.env.DB_NAME || 'test'
+const dbPass = process.env.DB_PASS || ''
+const dbUser = process.env.DB_USER || 'root'
+
+const connectionParams = {
+    charset: 'utf8',
+    db: dbName,
+    host: dbHost,
+    password: dbPass,
+    user: dbUser,
+}
+
+describe('immutable-core-task-instance run error check', function () {
+
+    var instance, instanceModel, task, taskModel, sandbox
+
+    var error1, errorCheck1, method1
+
+    // create database connection to use for testing
+    var database = new ImmutableDatabaseMariaSQL(connectionParams)
+    // fake session to use for testing
+    var session = {
+        accountId: '11111111111111111111111111111111',
+        roles: ['all', 'authenticated'],
+        sessionId: '22222222222222222222222222222222',
+    }
+
+    beforeEach(async function () {
+        sandbox = sinon.sandbox.create()
+        // create stubs for task methods
+        error1 = sandbox.stub()
+        errorCheck1 = sandbox.stub()
+        method1 = sandbox.stub()
+        // initialize models
+        var models = await initModels({database, session})
+        instanceModel = models.instanceModel
+        taskModel = models.taskModel
+        // create foo task
+        task = new ImmutableCoreTask({
+            instanceModel: instanceModel,
+            methods: {
+                error1: error1,
+                errorCheck1: errorCheck1,
+                method1: method1,
+            },
+            name: 'foo',
+            steps: [
+                {
+                    error: {
+                        check: {
+                            input: {foo: 'bar'},
+                            method: 'errorCheck1',
+                            output: {bar: 'foo'},
+                            retry: true,
+                        },
+                        method: 'error1',
+                        retry: true,
+                    },
+                    method: 'method1',
+                },
+            ],
+            taskModel: taskModel,
+        })
+        // sync task
+        await task.sync()
+        // create new instance
+        instance = await task.new({
+            foo: 1,
+            session: session,
+        })
+    })
+
+    afterEach(function () {
+        sandbox.restore()
+    })
+
+    after(function () {
+        database.close()
+    })
+
+    describe('when error check returns undefined', function () {
+
+        beforeEach(function () {
+            // method should always reject
+            method1.rejects()
+            // reject on first error handler call
+            error1.onCall(0).rejects()
+            // resolve on second 
+            error1.onCall(1).resolves()
+            // resolve check with undefined so error should be called again
+            errorCheck1.resolves();
+        })
+
+        it('should retry error', async function () {
+            // run task
+            await instance.run()
+            // reload instance data
+            await instance.reload()
+            // check data state
+            assert.isFalse(instance.record.data.complete)
+            assert.isUndefined(instance.record.data.success)
+            // run again
+            await instance.run()
+            // check that methods run
+            assert.calledOnce(method1)
+            assert.calledTwice(error1)
+            assert.calledOnce(errorCheck1)
+            assert.calledWithMatch(errorCheck1, {bar: 1})
+            // reload instance data
+            await instance.reload()
+            // check data state
+            assert.isTrue(instance.record.data.complete)
+            assert.isTrue(instance.record.data.success)
+        })
+
+    })
+
+    describe('when error check returns value', function () {
+
+        beforeEach(function () {
+            // method should always reject
+            method1.rejects()
+            // reject on error call
+            error1.rejects()
+            // resolve check with value so error should not be called again
+            errorCheck1.resolves({bar: 2})
+        })
+
+        it('should not retry error', async function () {
+            // run task
+            await instance.run()
+            // reload instance data
+            await instance.reload()
+            // check data state
+            assert.isFalse(instance.record.data.complete)
+            assert.isUndefined(instance.record.data.success)
+            // run again
+            await instance.run()
+            // check that methods run
+            assert.calledOnce(method1)
+            assert.calledOnce(error1)
+            assert.calledOnce(errorCheck1)
+            assert.calledWithMatch(errorCheck1, {bar: 1})
+            // reload instance data
+            await instance.reload()
+            // check data state
+            assert.isTrue(instance.record.data.complete)
+            assert.isTrue(instance.record.data.success)
+            assert.strictEqual(instance.record.data.data.foo, 2)
+        })
+
+    })
+
+    describe('when error check has error', function () {
+
+        beforeEach(function () {
+            // method should always reject
+            method1.rejects()
+            // reject on error call
+            error1.rejects()
+            // reject on first check call
+            errorCheck1.onCall(0).rejects()
+            // resolve check with value so error should not be called again
+            errorCheck1.onCall(1).resolves({bar: 2})
+        })
+
+        it('should retry error check', async function () {
+            // run task
+            await instance.run()
+            // reload instance data
+            await instance.reload()
+            // check data state
+            assert.isFalse(instance.record.data.complete)
+            assert.isUndefined(instance.record.data.success)
+            // run again
+            await instance.run()
+            // reload instance data
+            await instance.reload()
+            // run again
+            await instance.run()
+            // check that methods run
+            assert.calledOnce(method1)
+            assert.calledOnce(error1)
+            assert.calledTwice(errorCheck1)
+            // reload instance data
+            await instance.reload()
+            // check data state
+            assert.isTrue(instance.record.data.complete)
+            assert.isTrue(instance.record.data.success)
+            assert.strictEqual(instance.record.data.data.foo, 2)
+        })
+
+    })
+
+})
---
+++ test/run-reverse.js
@@ -0,0 +1,133 @@
+'use strict'
+
+/* npm modules */
+const ImmutableAI = require('immutable-ai')
+const ImmutableCore = require('immutable-core')
+const ImmutableDatabaseMariaSQL = require('immutable-database-mariasql')
+const ImmutableGlobal = require('immutable-global')
+const ImmutableCoreModel = require('immutable-core-model')
+const Promise = require('bluebird')
+const chai = require('chai')
+const sinon = require('sinon')
+
+/* application modules */
+const ImmutableCoreTask = require('../lib/immutable-core-task')
+const ImmutableCoreTaskInstance = require('../lib/immutable-core-task-instance')
+const initModels = require('./lib/init-models')
+
+/* chai config */
+const assert = chai.assert
+sinon.assert.expose(chai.assert, { prefix: '' })
+
+const dbHost = process.env.DB_HOST || 'localhost'
+const dbName = process.env.DB_NAME || 'test'
+const dbPass = process.env.DB_PASS || ''
+const dbUser = process.env.DB_USER || 'root'
+
+const connectionParams = {
+    charset: 'utf8',
+    db: dbName,
+    host: dbHost,
+    password: dbPass,
+    user: dbUser,
+}
+
+describe.only('immutable-core-task-instance run', function () {
+
+    var instance, instanceModel, task, taskModel, sandbox
+
+    var check1, error1, error3, errorCheck1, method1, method2, method3,
+        reverse1, reverseCheck1
+
+    // create database connection to use for testing
+    var database = new ImmutableDatabaseMariaSQL(connectionParams)
+    // fake session to use for testing
+    var session = {
+        accountId: '11111111111111111111111111111111',
+        roles: ['all', 'authenticated'],
+        sessionId: '22222222222222222222222222222222',
+    }
+
+    beforeEach(async function () {
+        sandbox = sinon.sandbox.create()
+        // create stubs for task methods
+        check1 = sandbox.stub()
+        error1 = sandbox.stub()
+        error3 = sandbox.stub()
+        errorCheck1 = sandbox.stub()
+        method1 = sandbox.stub()
+        method2 = sandbox.stub()
+        method3 = sandbox.stub()
+        reverse1 = sandbox.stub()
+        reverseCheck1 = sandbox.stub()
+        // initialize models
+        var models = await initModels({database, session})
+        instanceModel = models.instanceModel
+        taskModel = models.taskModel
+        // create foo task
+        task = new ImmutableCoreTask({
+            instanceModel: instanceModel,
+            methods: {
+                error3: error3,
+                method1: method1,
+                method2: method2,
+                method3: method3,
+                reverse1: reverse1,
+                reverseCheck1: reverseCheck1,
+            },
+            name: 'foo',
+            steps: [
+                {
+                    method: 'method1',
+                    reverse: 'reverse1',
+                },
+                {
+                    method: 'method2',
+                },
+                {
+                    error: 'error3',
+                    method: 'method3',
+                },
+            ],
+            taskModel: taskModel,
+        })
+        // sync task
+        await task.sync()
+        // create new instance
+        instance = await task.new({
+            session: session,
+        })
+    })
+
+    afterEach(function () {
+        sandbox.restore()
+    })
+
+    after(function () {
+        database.close()
+    })
+
+    describe('when method without error handler fails', function () {
+
+        beforeEach(function () {
+            method1.resolves()
+            method2.rejects()
+        })
+
+        it('should run reverse', async function () {
+            // run task
+            await instance.run()
+            // check that methods run
+            assert.calledOnce(method1)
+            assert.calledOnce(method2)
+            assert.calledOnce(reverse1)
+            // reload data
+            await instance.reload()
+            // check data state
+            assert.isTrue(instance.record.data.complete)
+            assert.isFalse(instance.record.data.success)
+        })
+
+    })
+
+})
